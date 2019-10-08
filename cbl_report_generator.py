ca4f4e571a46e684d180d1658f26869a451e107c
Matt <mwcooley99@gmail.com>
2019-10-08 15:36:48 -0700

    commits to outcome_avg and records tables

--- cbl_report_generator.py
+++ cbl_report_generator.py
@@ -12,6 +12,8 @@ from operator import itemgetter
 import time
 
 from pymongo import MongoClient
+import psycopg2
+# from config import db_config
 
 import asyncio
 import aiohttp
@@ -19,6 +21,32 @@ import aiohttp
 access_token = os.getenv('ACCESS_TOKEN')
 headers = {'Authorization': f'Bearer {access_token}'}
 url = 'https://dtechhs.instructure.com'
+
+
+# todo - move to it's own folder
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine
+
+
+username = 'TheDoctor'
+db_pass = os.getenv('DB_PASSWORD')
+
+Base = automap_base()
+engine = create_engine(f"postgresql+psycopg2://{username}:{db_pass}@localhost/cbldb")
+
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+# mapped classes
+OutcomeAverage = Base.classes.outcome_averages
+Record = Base.classes.records
+
+print(OutcomeAverage.__table__)
+
+
+
+
 
 
 def find_outcome(outcomes, outcome_id):
@@ -132,13 +160,59 @@ def append_rollup_details(data, course):
     return grade_rollup
 
 
+def get_outcome_rollups(course):
+    outcome_rollups = []
+    url = f"https://dtechhs.instructure.com/api/v1/courses/{course['id']}/outcome_rollups"
+    querystring = {"include[]": "outcomes", "per_page": "100"}
+    response = requests.request("GET", url, headers=headers,
+                                params=querystring)
+    # Pagination
+    outcome_rollups.append(response.json())
+    while response.links.get('next'):
+        url = response.links['next']['url']
+        response = requests.request("GET", url, headers=headers,
+                                    params=querystring)
+        outcome_rollups.append(response.json())
+
+    return outcome_rollups
+
+
+def extract_outcome_avg_data(score, course, user_id, record_id):
+    print('******')
+    print(score)
+    outcome_avg = OutcomeAverage(
+        user_id = user_id,
+        course_id = course['id'],
+        outcome_id = score['links']['outcome'],
+        outcome_avg = score['score'],
+        record_id = record_id,
+        course_name = course['name']
+    )
+    course_id = course['id']
+    outcome_id = score['links']['outcome']
+
+
+    return outcome_avg
+
+
+
+
 def main():
+    session = Session(engine)
     # Get current term(s) - todo search for all terms within a date. Will return a list if more than one term
     current_term = 10
 
+    # Create a new record
+    timestamp = datetime.utcnow()
+    record = Record(created_at=timestamp, term_id=current_term)
+    session.add(record)
+    session.commit()
+
+
+
     # get all active courses
     url = "https://dtechhs.instructure.com/api/v1/accounts/1/courses"
-    querystring = {'enrollment_term_id': 10, 'published': True,
+    querystring = {'enrollment_term_id': current_term, 'published': True,
                    'per_page': 100}
     response = requests.request("GET", url, headers=headers,
                                 params=querystring)
@@ -152,50 +226,45 @@ def main():
         courses += response.json()
 
     # get outcome result rollups for each course - attach to the course dictionary
-    outcome_grades = []
+    outcome_averages = []
     pattern = '@dtech|Innovation Diploma FIT'
-    for course in courses:
+    for course in courses[:1]:
         if re.search(pattern, course['name']):
             continue
         # Get the outcome_rollups for the current class
-        outcome_rollups = []
-        url = f"https://dtechhs.instructure.com/api/v1/courses/{course['id']}/outcome_rollups"
-        querystring = {"include[]": "outcomes", "per_page": "100"}
-        response = requests.request("GET", url, headers=headers,
-                                    params=querystring)
-        # Pagination
-        outcome_rollups.append(response.json())
-        while response.links.get('next'):
-            url = response.links['next']['url']
-            response = requests.request("GET", url, headers=headers,
-                                        params=querystring)
-            outcome_rollups.append(response.json())
-
+        outcome_rollups_list = get_outcome_rollups(course)
         # Make the student Objects
-        for outcome_rollup in outcome_rollups:
-            outcome_grades += append_rollup_details(outcome_rollup, course)
-
-    timestamp = datetime.utcnow()
-
-    # group by student
-    data_sorted = sorted(outcome_grades, key=itemgetter('student_id'))
-    grouped = itertools.groupby(data_sorted, key=itemgetter('student_id'))
-    student_objects = [{'student_id': student_id, 'timestamp': timestamp,
-                        'term_id': current_term,
-                        'courses': sorted(list(values),
-                                          key=itemgetter('course_name'))} for
-                       student_id, values in grouped]
-
-    # Load into database
-    client = MongoClient('localhost', 27017)
-    db = client.test_database
-    grades = db.grades
-    grades.insert_many(student_objects)
+        outcome_averages = []
+        for outcome_rollups in outcome_rollups_list:
+            for student_rollup in outcome_rollups['rollups']:
+                print(student_rollup)
+                user_id = student_rollup['links']['user']
+                outcome_averages += [extract_outcome_avg_data(score, course, user_id, record.id) for score in student_rollup['scores']]
+        session.add_all(outcome_averages)
+        session.commit()
+    #
+    # print(outcome_rollups)
+    # timestamp = datetime.utcnow()
+    #
+    # # group by student
+    # data_sorted = sorted(outcome_grades, key=itemgetter('student_id'))
+    # grouped = itertools.groupby(data_sorted, key=itemgetter('student_id'))
+    # student_objects = [{'student_id': student_id, 'timestamp': timestamp,
+    #                     'term_id': current_term,
+    #                     'courses': sorted(list(values),
+    #                                       key=itemgetter('course_name'))} for
+    #                    student_id, values in grouped]
+    #
+    # # Load into database
+    # client = MongoClient('localhost', 27017)
+    # db = client.test_database
+    # grades = db.grades
+    # grades.insert_many(student_objects)
 
 
 if __name__ == '__main__':
     start = time.time()
-    # main()
-    get_outcomes()
+    main()
+    # get_outcomes()
     end = time.time()
     print(end - start)
--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import settings
+from configparser import ConfigParser
 
 
 class Config(object):
@@ -24,3 +25,22 @@ class TestingConfig(BaseConfig):
     PYLTI_CONFIG = settings.PYLTI_CONFIG
 
 # DEFINE ADDITIONAL CONFIGS AS NEEDED
+
+
+# def db_config(filename='database.ini', section='postgresql'):
+#     # create a parser
+#     parser = ConfigParser()
+#     # read config file
+#     parser.read(filename)
+#
+#     # get section, default to postgresql
+#     db = {}
+#     if parser.has_section(section):
+#         params = parser.items(section)
+#         for param in params:
+#             db[param[0]] = param[1]
+#     else:
+#         raise Exception(
+#             'Section {0} not found in the {1} file'.format(section, filename))
+#
+#     return db

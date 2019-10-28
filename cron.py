81ac616cd606dd7a8306648252870c6c03e96520
Matt <mwcooley99@gmail.com>
2019-10-28 16:51:27 -0700

    updated the cron job to run at Tues 11pm PST

--- cron.py
+++ cron.py
@@ -1,15 +1,16 @@
 from pytz import utc
+from datetime import datetime
 from apscheduler.schedulers.blocking import BlockingScheduler
 
 sched = BlockingScheduler(timezone=utc)
 
 
-@sched.scheduled_job('cron', day_of_week="mon", hour='23', minute=47)
+@sched.scheduled_job('cron', day_of_week="tues", hour='6', minute=00)
 def timed_job():
-    # update_users()
-    print("users updated")
-    # preform_grade_pull()
-    print("grades pulled")
+    update_users()
+    print(f"users updated at {datetime.now()}")
+    preform_grade_pull()
+    print(f"grades pulled at {datetime.now()}")
 
 
 from data_pull import update_users, preform_grade_pull

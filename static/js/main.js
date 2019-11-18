29294bdb4134f52a2aed586cca95c2081e6004ce
Matt <mwcooley99@gmail.com>
2019-11-17 17:38:15 -0800

    added student search filter to course dashboard

--- static/js/main.js
+++ static/js/main.js
@@ -9,26 +9,6 @@ function showHide(elem) {
     document.getElementById("accordion"+sel).style.display = 'block';
 }
 
-// function filterStudents() {
-//   // Declare variables
-//   var input, filter, ul, li, a, i, txtValue;
-//   input = document.getElementById('myInput');
-//   filter = input.value.toUpperCase();
-//   ul = document.getElementById("myUL");
-//   li = ul.getElementsByTagName('li');
-//
-//   // Loop through all list items, and hide those who don't match the search query
-//   for (i = 0; i < li.length; i++) {
-//     a = li[i].getElementsByTagName("a")[0];
-//     txtValue = a.textContent || a.innerText;
-//     if (txtValue.toUpperCase().indexOf(filter) > -1) {
-//       li[i].style.display = "";
-//     } else {
-//       li[i].style.display = "none";
-//     }
-//   }
-// }
-
 $(document).ready(function(){
   $("#myInput").on("keyup", function() {
     var value = $(this).val().toLowerCase();

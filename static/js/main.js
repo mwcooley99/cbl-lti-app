a2533cfa6a47ddf60d4dd837e7e563a805508619
Matt <mwcooley99@gmail.com>
2019-11-18 08:40:03 -0800

    increased session timeout

--- static/js/main.js
+++ static/js/main.js
@@ -5,15 +5,26 @@ function showHide(elem) {
     }
     //unhide the selected div
     var sel = document.getElementById('sel').value;
-    console.log("accordion"+sel);
-    document.getElementById("accordion"+sel).style.display = 'block';
+    console.log("accordion" + sel);
+    document.getElementById("accordion" + sel).style.display = 'block';
 }
 
-$(document).ready(function(){
-  $("#myInput").on("keyup", function() {
-    var value = $(this).val().toLowerCase();
-    $(".student-name").filter(function() {
-      $(this).closest(".card").toggle($(this).text().toLowerCase().indexOf(value) > -1)
+$(document).ready(function () {
+    $("#myInput").on("keyup", function () {
+        var value = $(this).val().toLowerCase();
+        $(".student-name").filter(function () {
+            $(this).closest(".card").toggle($(this).text().toLowerCase().indexOf(value) > -1)
+        });
     });
-  });
 });
+
+var $btns = $('.btn').click(function () {
+    if (this.id == 'all') {
+        $('#parent > div').fadeIn(450);
+    } else {
+        var $el = $('.' + this.id).fadeIn(450);
+        $('#parent > div').not($el).hide();
+    }
+    $btns.removeClass('active');
+    $(this).addClass('active');
+})

6854bfc79766ccfc0f01b78631f5816ee50a5c94
Matt <mwcooley99@gmail.com>
2019-11-23 06:25:29 -0800

    updated cron job to run each weekday

--- cron.py
+++ cron.py
@@ -2,17 +2,17 @@ from pytz import utc
 from datetime import datetime
 from apscheduler.schedulers.blocking import BlockingScheduler
 
+from data_pull import update_users, preform_grade_pull
+
 sched = BlockingScheduler(timezone=utc)
 
 
-@sched.scheduled_job('cron', day_of_week="tue", hour=22, minute=5)
+@sched.scheduled_job('cron', day_of_week="mon-fri", hour=14)
 def timed_job():
     update_users()
     print(f"users updated at {datetime.now()}")
     preform_grade_pull()
     print(f"grades pulled at {datetime.now()}")
 
-
-from data_pull import update_users, preform_grade_pull
 
 sched.start()

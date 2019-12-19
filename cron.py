6714e1f7aad300c07904ce1f8e3789d48e841b4a
Matt <mwcooley99@gmail.com>
2019-12-19 05:57:38 -0800

    cron update

--- cron.py
+++ cron.py
@@ -7,12 +7,13 @@ from data_pull import update_users, preform_grade_pull
 sched = BlockingScheduler(timezone=utc)
 print(datetime.now())
 
-@sched.scheduled_job('cron', day_of_week="mon-fri", hour=9)
+
+@sched.scheduled_job('cron', day_of_week="mon-fri", hour=14)
 def timed_job():
     print(f'job started at {datetime.now()}')
-    update_users()
+    # update_users()
     print(f"users updated at {datetime.now()}")
-    preform_grade_pull()
+    # preform_grade_pull()
     print(f"grades pulled at {datetime.now()}")
 
 

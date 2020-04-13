1f690daf75e2c1eb17fe253601462e90603e0818
Matt <mwcooley99@gmail.com>
2020-04-13 16:03:44 -0700

    formatting

--- cron.py
+++ cron.py
@@ -3,18 +3,24 @@ from datetime import datetime
 from apscheduler.schedulers.blocking import BlockingScheduler
 from pytz import utc
 
-from utilities.data_pull import update_users, pull_outcome_results, \
-    insert_grades, update_courses, update_course_students, update_terms
+from utilities.data_pull import (
+    update_users,
+    pull_outcome_results,
+    insert_grades,
+    update_courses,
+    update_course_students,
+    update_terms,
+)
 
 from utilities.db_functions import get_current_term
 
 sched = BlockingScheduler(timezone=utc)
 
 
-@sched.scheduled_job('cron', day_of_week="mon-fri", hour=8, minute=5)
+@sched.scheduled_job("cron", day_of_week="mon-fri", hour=8, minute=5)
 def timed_job():
 
-    print(f'job started at {datetime.now()}')
+    print(f"job started at {datetime.now()}")
     update_terms()
     print(f"terms updated at {datetime.now()}")
     # get current term
@@ -32,6 +38,6 @@ def timed_job():
     print(f"grades inserted at {datetime.now()}")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     print(get_current_term())
     sched.start()

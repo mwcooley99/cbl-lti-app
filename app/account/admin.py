87eca1b4b893296717bd23d24eb584f3fa785645
Matt <mwcooley99@gmail.com>
2021-01-02 20:12:44 -0800

    add descriptions to admin terms

--- app/account/admin.py
+++ app/account/admin.py
@@ -7,8 +7,8 @@ from app.models import EnrollmentTerm, GradeCalculation, CanvasApiToken
 
 class CblModelView(ModelView):
     def is_accessible(self):
-        if 'role' in session:
-            return session['role'] == 'Admin'
+        if "role" in session:
+            return session["role"] == "Admin"
         return False
 
     def inaccessible_callback(self, name, **kwargs):
@@ -19,23 +19,34 @@ class CblModelView(ModelView):
 class EnrollmentTermView(CblModelView):
     can_create = False
     can_delete = False
-    column_list = ('name', 'current_term', 'sync_term', 'cut_off_date')
-    column_editable_list = ['current_term', 'sync_term', 'cut_off_date']
+    column_list = ("name", "current_term", "sync_term", "cut_off_date")
+    column_editable_list = ["current_term", "sync_term", "cut_off_date"]
 
-    form_excluded_columns = ['id', 'name', 'start_at', 'end_at', 'created_at',
-                             'workflow_state', 'sis_term_id', 'sis_import_id']
+    form_excluded_columns = [
+        "id",
+        "name",
+        "start_at",
+        "end_at",
+        "created_at",
+        "workflow_state",
+        "sis_term_id",
+        "sis_import_id",
+    ]
+
+    column_descriptions = dict(
+        sync_term=(
+            "If true, the nightly sync will update the terms courses, outcome results, grades, etc. "
+            "Only select columns that need to be synced for performance. Multiple terms can be selected"
+        ),
+        current_term="The term that will be displayed in the student/observer view. ONLY ONE can be selected at a time.",
+    )
 
 
 class GradeCriteriaView(CblModelView):
     column_descriptions = dict(
-        grade_rank='1 = Highest grade. This order must be correct for grades to calculate correctly'
+        grade_rank="1 = Highest grade. This order must be correct for grades to calculate correctly"
     )
-
-
-class CanvasApiTokenView(CblModelView):
-    pass
 
 
 admin.add_view(EnrollmentTermView(EnrollmentTerm, db.session))
 admin.add_view(GradeCriteriaView(GradeCalculation, db.session))
-# admin.add_view(CanvasApiTokenView(CanvasApiToken, db.session))

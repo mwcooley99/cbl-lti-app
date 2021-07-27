66fd117a35564b2c854f4734435deffd40789227
Matt <mwcooley99@gmail.com>
2021-07-26 23:28:14 -0400

    hello world for api blueprint and clean up migrations

---
+++ app/api/views.py
@@ -0,0 +1,39 @@
+import time
+
+from flask import (
+    Blueprint,
+    render_template,
+    current_app,
+    session,
+    url_for,
+    request,
+    redirect,
+)
+
+from app.extensions import db
+from app.models import (
+    Outcome,
+    Course,
+    Record,
+    Grade,
+    User,
+    OutcomeResult,
+    CourseUserLink,
+    OutcomeResultSchema,
+    OutcomeSchema,
+    EnrollmentTerm,
+)
+from app.queries import get_calculation_dictionaries
+
+from utilities.canvas_api import get_course_users
+
+# from utilities.cbl_calculator import calculation_dictionaries
+from utilities.helpers import make_outcome_avg_dicts, format_users, error
+
+blueprint = Blueprint(
+    "api", __name__, url_prefix="/api"
+)
+
+@blueprint.route("/hello_world")
+def hello():
+    return "Hello world"

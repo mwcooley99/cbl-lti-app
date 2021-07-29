392828a6ede5a20ff17a662ae92dee7a4eec17e7
Matt <mwcooley99@gmail.com>
2021-07-28 20:53:00 -0400

    add mvp for grade dump api

--- app/api/views.py
+++ app/api/views.py
@@ -1,4 +1,5 @@
 import time
+import jwt
 
 from flask import (
     Blueprint,
@@ -8,8 +9,9 @@ from flask import (
     url_for,
     request,
     redirect,
+    jsonify
 )
-
+from app.queries import get_calculation_dictionaries, get_enrollment_term
 from app.extensions import db
 from app.models import (
     Outcome,
@@ -40,4 +42,23 @@ def hello():
 
 @blueprint.route("/dragon_time", methods=["POST", "GET"])
 def dragon_time():
-    pass
+    key = "secret"
+    enrollment_term = get_enrollment_term()
+    stmt = db.text(
+        """
+        SELECT
+            u.id
+            , u.login_id
+            , g.grade
+            , c.name as course_name
+        FROM grades g
+            JOIN courses c on c.id = g.course_id
+            JOIN users u on u.id = g.user_id
+        WHERE c.enrollment_term_id = :enrollment_term_id
+        ORDER BY g.id
+        """
+    ).bindparams(enrollment_term_id=enrollment_term.id)
+    results = db.session.execute(stmt)
+    data = [dict(row) for row in results]
+    encoded = jwt.encode({'data': data}, key, algorithm="HS256")
+    return jsonify(encoded)
